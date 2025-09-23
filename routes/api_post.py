8f1d6f7428ded5fcee4f0071e9c3bb18c4ca988c
gustavo rodrigues <gustavolourenco821@gmail.com>
2025-09-23 14:47:17 -0300

    add: rotas para cadastrar de novo catador

--- routes/api_post.py
+++ routes/api_post.py
@@ -1,3 +1,12 @@
-from flask import redirect, Blueprint
+from flask import redirect, Blueprint, request
 
 api_post = Blueprint('api_post', __name__, url_prefix="/post")
+
+@api_post.routes("/catador", methods=["POST"])
+def cadastrar_catador():
+    cpf = request.form.get("cpf")
+    nome = request.form.get("nome")
+    telefone = request.form.get("telefone")
+    data_nascimento = request.form.get("data")
+    # Catadores.cadastrar(cpf, nome, telefone, data_nascimento)
+
