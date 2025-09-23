48cdb8708118af2644fd05dffbbff222ba36075f
Samuel Moreira Ferreira <samuel.mferreira205@gmail.com>
2025-09-23 16:48:13 -0300

    ADD Classe Cooperativas
    ADD Cooperativas -> '.get_by_cnpj()'

---
+++ controllers/cooperativa_controller.py
@@ -0,0 +1,15 @@
+class Cooperativas:
+
+    def get_by_cnpj (cnpj):
+
+        """
+        Consulta o CNPJ e retorna a cooperativa
+        com o CNPJ requisitado, caso registrado,
+        se não 'null'
+        """
+
+        pass
+
+
+
+        
