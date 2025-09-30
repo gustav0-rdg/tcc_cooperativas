066653b7c9c1b33faa8631acc31a8acab6f6f411
gustavo rodrigues <gustavolourenco821@gmail.com>
2025-09-30 07:53:30 -0300

    add: rotas para entrar nas paginas html utilizando flask

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from routes.pages import pages
-from routes.auth.auth_routes import auth
-from routes.api_post import api_post
+# from routes.auth.auth_routes import auth
+# from routes.api_post import api_post
 
 app = Flask(__name__)
 
 app.register_blueprint(pages)
-app.register_blueprint(auth)
-app.register_blueprint(api_post)
+# app.register_blueprint(auth)
+# app.register_blueprint(api_post)
 
 app.run(debug=True)
