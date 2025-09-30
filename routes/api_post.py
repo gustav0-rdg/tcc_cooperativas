3bb992b31983ae9a151d0fbefab9d3fe1bf4dc4a
gustavo rodrigues <gustavolourenco821@gmail.com>
2025-09-30 08:55:41 -0300

    add: sweet alert enviando se o cnpj foi válido ou não

--- routes/api_post.py
+++ routes/api_post.py
@@ -1,12 +1,11 @@
-# from flask import redirect, Blueprint, request
+from flask import redirect, Blueprint, request
 
-# api_post = Blueprint('api_post', __name__, url_prefix="/post")
+api_post = Blueprint('api_post', __name__, url_prefix="/post")
 
-# @api_post.route("/catador", methods=["POST"])
-# def cadastrar_catador():
-#     cpf = request.form.get("cpf")
-#     nome = request.form.get("nome")
-#     telefone = request.form.get("telefone")
-#     data_nascimento = request.form.get("data")
-    # Catadores.cadastrar(cpf, nome, telefone, data_nascimento)
-
+@api_post.route("/cooperativa", methods=["POST"])
+def cadastrar_cooperativa():
+    cnpj = request.form.get("cnpj")
+    nome = request.form.get("nome")
+    telefone = request.form.get("telefone")
+    data_nascimento = request.form.get("data")
+    return redirect("/login")
