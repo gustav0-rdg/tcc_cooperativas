8a0b0f3081b0a5e44464f50d8ab7787e14cb9a2b
gustavo rodrigues <gustavolourenco821@gmail.com>
2025-10-21 15:43:05 -0300

    ADD: acessando a funcao que busca os compradores

--- static/js/api/getCompradores.js
+++ static/js/api/getCompradores.js
@@ -3,3 +3,15 @@ export async function getCompradores() {
     const data = await response.json()
     return data
 }
+
+// Função para buscar os compradores pelo NOME do material
+export async function getCompradoresPorMaterial(nome_material) {
+    try {
+        const response = await fetch(`/get/comprador/${nome_material}`);            
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Falha na requisição fetch:", error);
+        return [];
+    }
+}
