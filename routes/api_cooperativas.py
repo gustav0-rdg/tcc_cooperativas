22fe85b6162ecdda3ee4e7cdde59d6ad9ff0316b
Samuel Moreira Ferreira <samuel.mferreira205@gmail.com>
2025-10-28 08:44:37 -0300

    ADD API Cooperativas: Rota para cadastro

--- routes/api_cooperativas.py
+++ routes/api_cooperativas.py
@@ -3,6 +3,7 @@ from controllers.tokens_controller import Tokens
 from data.connection_controller import Connection
 from controllers.usuarios_controller import Usuarios
 from controllers.cooperativa_controller import Cooperativa
+from controllers.cnpj_controller import CNPJ
 
 api_cooperativas = Blueprint(
     
@@ -12,6 +13,174 @@ api_cooperativas = Blueprint(
     url_prefix='/api/cooperativas'
     
 )
+
+CNAES_PERMITIDOS = [3811400, 3831901, 3831999, 3832700, 3812200]
+NATUREZAS_JURIDICAS_PERMITIDAS = [2143, 3999] 
+
+@api_cooperativas.route('/cadastrar', methods=['POST'])
+def cadastrar ():
+
+    conn = None
+
+    try:
+
+        data_cadastro = request.get_json()
+        campos_obrigatorios = ['nome', 'email', 'senha', 'cnpj']
+
+        # 400 - Campos obrigatórios incompletos
+
+        if not data_cadastro or not all(key in data_cadastro for key in campos_obrigatorios):
+            return jsonify({ 'error': f'Dados de cadastro inválidos, todos os campos são obrigatórios: {campos_obrigatorios}' }), 400
+
+        # 400 - Senha com menos de 8 caractéres
+
+        if len(data_cadastro['senha']) < 8:
+            return jsonify({ 'texto': 'A senha deve ter no minímo 8 caractéres' }), 400
+        
+        # 400 - CNPJ Inválido
+
+        if CNPJ.validar(data_cadastro['cnpj']):
+            return jsonify({ 'error': 'CNPJ inválido. Revise e tente novamente' }), 400
+
+        dados_cnpj = CNPJ.consultar(data_cadastro['cnpj'])
+
+        # 503 - Erro ao consultar CNPJ (CNPJá)
+
+        if not dados_cnpj:
+            return jsonify({'error': 'Não foi possível consultar o CNPJ. Tente novamente mais tarde.'}), 503
+        
+        #region Verificação de Natureza Jurídica
+
+        natureza_cooperativa = dados_cnpj.get('company', {}).get('nature', {}).get('id')
+
+        # 400 - Código de Natureza Jurídica Inválido
+
+        if not natureza_cooperativa in NATUREZAS_JURIDICAS_PERMITIDAS:
+            return jsonify({'error': 'O CNPJ informado não pertence a uma Cooperativa ou Associação.'}), 400
+        
+        #endregion
+
+        #region Verificação de CNAE
+
+        cnae_principal_id = dados_cnpj.get('mainActivity', {}).get('id')
+        cnaes_secundarios_ids = [act.get('id') for act in dados_cnpj.get('sideActivities', [])]
+
+        cnae_valido = False
+
+        if cnae_principal_id in CNAES_PERMITIDOS:
+            cnae_valido = True
+
+        else:
+
+            for cnae_sec_id in cnaes_secundarios_ids:
+
+                if cnae_sec_id in CNAES_PERMITIDOS:
+                    cnae_valido = True
+                    break
+
+        # 400 - CNAE Inválido
+
+        if not cnae_valido:
+            return jsonify({'error': 'O CNPJ informado não possui CNAE compatível com cooperativas de reciclagem.'}), 400
+
+        #endregion
+
+        id_status_atividade_cooperativa = dados_cnpj.get('status', {}).get('id')
+
+        # IDS de Status:
+        #
+        # 1 = Nulo
+        # 2 = Ativa
+        # 3 = Suspensa
+        # 4 = Inapta
+        # 8 = Baixada
+
+        # 400 - CNPJ Inativo
+
+        if id_status_atividade_cooperativa != 2:
+            return jsonify({'error': 'O CNPJ informado está inativo.'}), 400
+
+        conn = Connection('online')
+
+        conn.connection_db.start_transaction()
+        print("Transação iniciada")
+
+        id_usuario_criado = Usuarios(conn.connection_db).create(
+
+            nome=data_cadastro['nome'],
+            email=data_cadastro['email'],
+            senha=data_cadastro['senha'],
+            tipo='cooperativa'
+
+        )
+
+        match id_usuario_criado:
+
+            # 409 - Email já cadastrado
+
+            case None:
+                return jsonify({'error': 'Este email já está cadastrado.'}), 409
+            
+            # Usuário da cooperativa criado
+
+            case _ if isinstance(id_usuario_criado, int):
+
+                addr = dados_cnpj.get('address', {})
+                endereco_completo = f"{addr.get('street', '')}, {addr.get('number', '')} {addr.get('details', '')} - {addr.get('district', '')}"
+
+                id_cooperativa_criada = Cooperativa(conn.connection_db).create(
+
+                    id_usuario=id_usuario_criado,
+                    cnpj=data_cadastro['cnpj'],
+                    razao_social=dados_cnpj.get('company', {}).get('name', 'Razão Social não encontrada'),
+                    endereco=endereco_completo.strip(" ,-"),
+                    cidade=addr.get('city'),
+                    estado=addr.get('state')
+                    # Latitude e longitude (não essencial por agora)
+
+                )
+
+                match id_cooperativa_criada:
+
+                    # 409 - CNPJ já cadastrado
+
+                    case None:
+                        return jsonify({'error': 'Este CNPJ já está cadastrado.'}), 409
+
+                    # 201 - Cooperativa criada pendente e aguardando aprovação
+
+                    case _ if isinstance(id_cooperativa_criada, int):
+                        return jsonify({'texto': 'Cadastro realizado com sucesso! Sua conta está pendente e está aguardando aprovação.'}), 201
+
+                    # 500 - Erro Interno
+
+                    case False | _:
+                        return jsonify({'error': 'Ocorreu um erro interno no servidor durante o cadastro.'}), 500
+
+            # 500 - Erro Interno
+
+            case False | _:
+                return jsonify({'error': 'Ocorreu um erro interno no servidor durante o cadastro.'}), 500
+    
+    except Exception as e:
+
+        if conn and conn.connection_db.is_connected():
+
+            try:
+
+                if conn.connection_db.in_transaction():
+                    conn.connection_db.rollback()
+
+            except Exception as rollback_err:
+
+                print(f"Erro durante o rollback: {rollback_err}")
+
+        return jsonify({'error': 'Ocorreu um erro interno no servidor durante o cadastro.'}), 500
+    
+    finally:
+
+        if conn:
+            conn.close()
 
 @api_cooperativas.route('/alterar-aprovacao/<id_cooperativa>', methods=['POST'])
 def alterar_aprovacao (id_cooperativa:int):
