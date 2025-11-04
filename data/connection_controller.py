b5bd676f6e7c4459d4bf723c32aacc5a0b96fa0f
gustavo rodrigues <gustavolourenco821@gmail.com>
2025-11-04 10:41:32 -0300

    FIX: sla mano

--- data/connection_controller.py
+++ data/connection_controller.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from mysql.connector import MySQLConnection, CMySQLConnection
+from mysql.connector import MySQLConnection
 
 info_conexoes = {
 
@@ -33,7 +33,6 @@ class Connection:
     connection_db: MySQLConnection = None
 
     def __init__(self, tipo_conexao: str):
-        tipo_conexao = 'online' # Ajustar conforme uso!!!!!!!
         if tipo_conexao not in info_conexoes:
             raise ValueError(f'Erro - Connection: Valor de "tipo_conexao" não é válido: {tipo_conexao}')
 
@@ -72,8 +71,8 @@ class Connection:
              return True 
 
     @staticmethod
-    def validar (connection_db:MySQLConnection|CMySQLConnection) -> bool:
-        if isinstance(connection_db, (MySQLConnection, CMySQLConnection)) and connection_db.is_connected():
+    def validar (connection_db:MySQLConnection) -> bool:
+        if isinstance(connection_db, (MySQLConnection)) and connection_db.is_connected():
             return True
         else:
             return False
