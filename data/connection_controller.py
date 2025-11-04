aba4d1ee7dc30396f0261c5c745adaf48c5a3f8f
Miguel CÂamara <miguelll.camara@gmail.com>
2025-11-04 08:27:59 -0300

    FIX: tipo de conexão alterada para a database no azure

--- data/connection_controller.py
+++ data/connection_controller.py
@@ -33,6 +33,7 @@ class Connection:
     connection_db: MySQLConnection = None
 
     def __init__(self, tipo_conexao: str):
+        tipo_conexao = 'online' # Ajustar conforme uso!!!!!!!
         if tipo_conexao not in info_conexoes:
             raise ValueError(f'Erro - Connection: Valor de "tipo_conexao" não é válido: {tipo_conexao}')
 
