d58ec6249088af2141d669d546adff8781676af1
ZenonPB <zenon.bergamo321@gmail.com>
2025-11-05 10:25:41 -0300

    FIX Conexão online na Azure

--- data/connection_controller.py
+++ data/connection_controller.py
@@ -28,6 +28,7 @@ class Connection:
     """
 
     def __init__(self, tipo_conexao: str = 'local'):
+        tipo_conexao='online'
         if tipo_conexao not in info_conexoes:
             raise ValueError(f'Erro - Connection: Valor de "tipo_conexao" não é válido: {tipo_conexao}')
 
