c1b47197e6fe1969d8d3cccc050eb32ef3f493e9
Samuel Moreira Ferreira <samuel.mferreira205@gmail.com>
2025-11-17 13:37:54 -0300

    ADD Cálculo de Haversine para medir distância entre dois pontos com latitude e longitude

--- controllers/endereco_controller.py
+++ controllers/endereco_controller.py
@@ -1,5 +1,6 @@
 import urllib.parse
 import requests
+import math
 
 class Endereco:
 
@@ -42,3 +43,30 @@ class Endereco:
 
             print(f"Erro ao chamar a API da Nominatim: {e}")
             return None
+        
+    @staticmethod        
+    def haversine(lat1, lon1, lat2, lon2):
+
+        """
+        A fórmula de Haversine é um cálculo matemático usado para determinar a 
+        distância entre dois pontos na superfície da Terra usando latitude e longitude.
+        Ela leva em conta a curvatura da Terra, garantindo que a distância seja 
+        muito mais precisa do que uma simples conta em linha reta no plano.
+        """
+
+        # Converte as coordenadas de graus para radianos
+        lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
+
+        # Diferenças entre as coordenadas
+        dlat = lat2 - lat1
+        dlon = lon2 - lon1
+
+        # Fórmula de Haversine
+        a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+        # Raio da Terra (em quilômetros)
+        R = 6371.0
+
+        # Distância em quilômetros
+        return R * c
