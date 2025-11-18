d007814c2a121a21c408718ad083d0863223e4e7
Gustavo <gustavolourenco821@gmail.com>
2025-11-17 22:21:48 -0300

    ADD: rota pra excluir um cooperado

--- routes/api_cooperados.py
+++ routes/api_cooperados.py
@@ -43,6 +43,27 @@ def get_cooperados(identificador: int):
     finally:
         conn.close()
 
+@api_cooperados.route('/delete/<id_cooperado>')
+def delete_cooperado(id_cooperado):
+    token = request.headers.get('authorization')
+    if not token:
+        return jsonify({'erro':'"Token" é um parametro obrigatório'}),400
+    conn = Connection('local')
+    try:
+        data_token = Tokens(conn.connection_db).validar(token)
+        if not data_token or data_token['tipo'] != 'sessao':
+            return jsonify({'error':'"Token" inexistente ou inválido'}),400
+        user = Usuarios(conn.connection_db).get(data_token['id_usuario'])
+        if not user['tipo'] in ['cooperativa', 'gestor', 'root']:
+            return jsonify({'error':'você não tem permissão pra realizar tal ação'})
+        elif user['tipo'] == 'cooperativa':
+            
+    except Exception as e:
+        return jsonify({'error': f'Erro no servidor {e}'}),500
+    
+    finally:
+        conn.close()    
+
 @api_cooperados.route('/get/<identificador>/<nome>', methods=["POST"])
 def search_cooperado(identificador, nome):
     token = request.headers.get('authorization')
